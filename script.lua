a2b3cd7d61158b71feaa8cef7699ca51f8d684d5
AkaraponThugNoLuv <allday147z@gmail.com>
2025-02-16 23:35:45 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -19,7 +19,7 @@ if player:FindFirstChild("PlayerStats") and player.PlayerStats:FindFirstChild("S
             ["embeds"] = {
                 {
                     ["title"] = "แจ้งเตือนสุ่ม Style",  -- ชื่อของ Embed
-                    ["description"] = player.Name .. " ได้รับ Style: " .. styleValue.Value,  -- คำอธิบาย
+                    ["description"] ="**ชื่อตัวละคร** ||" .. player.Name .. "|| ได้รับสไตล์: " .. styleValue.Value,
                     ["color"] = 0x00FF00,  -- สีของ Embed (Green)
                     ["image"] = {
                         ["url"] = "https://media.discordapp.net/attachments/1285600624666476605/1340717423338197174/13c48f8c1fa28ec3cc188f1e639ad2b8.gif?ex=67b35fe7&is=67b20e67&hm=2179ac729f769fe2d8a16fdf69ed6acade6a0215268c93bc972ca6650ae96f61&="
