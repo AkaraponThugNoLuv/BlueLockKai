6db67a3676412ac048759b6c18972dcaef0abd12
AkaraponThugNoLuv <allday147z@gmail.com>
2025-03-05 12:49:32 +0700

    Update stylekai.lua

--- stylekai.lua
+++ stylekai.lua
@@ -86,7 +86,7 @@ end
 local function checkStyle()
     if player:FindFirstChild("PlayerStats") and player.PlayerStats:FindFirstChild("Style") then
         local styleValue = player.PlayerStats.Style.Value
-        local styleneed = {"Don Lorenzo"}
+        local styleneed = {"Sae"}
 
         -- ตรวจสอบว่า styleValue เป็นค่าที่ต้องการหรือไม่
         if table.find(styleneed, styleValue) then
