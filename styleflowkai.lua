780dc4588f96623693da840788649d6aaea93006
AkaraponThugNoLuv <allday147z@gmail.com>
2025-03-07 22:23:11 +0700

    Update styleflowkai.lua

--- styleflowkai.lua
+++ styleflowkai.lua
@@ -105,7 +105,7 @@ local function checkStyle()
                     local success = sendWebhook(styleValue, flowValue)
                     if success then
                         -- เตะผู้เล่นหลังจากส่ง Webhook สำเร็จ
-                        wait(30)
+                        wait(60)
                         player:Kick("คุณได้รับสไตล์ " .. styleValue .. " และ Flow " .. flowValue .. " แล้ว")
                     end
                 else
