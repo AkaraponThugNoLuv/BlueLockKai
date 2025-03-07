db888a739f70ae656a4206e375552b4857ffebfc
AkaraponThugNoLuv <allday147z@gmail.com>
2025-03-07 22:22:54 +0700

    Update stylekai.lua

--- stylekai.lua
+++ stylekai.lua
@@ -103,7 +103,7 @@ local function checkStyle()
                 -- ส่ง Webhook
                 local success = sendWebhook(styleValue)
                 if success then
-                    wait(30)
+                    wait(60)
                     -- เตะผู้เล่นหลังจากส่ง Webhook สำเร็จ
                     player:Kick("คุณได้รับสไตล์ " .. styleValue .. " แล้ว ")
                 end
